08391152477bde6dadcb53355a0da36b2c9b0707
loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-02-21 13:06:26 +0100

    Update cake.py

---
+++ classe/cake.py
@@ -0,0 +1,8 @@
+class Cake:
+    def __init__(ingredients=dict(), temps_cuisson=int(), unit=str()):
+        self.ingredients=ingredients
+        self.temps_cuisson=temps_cuisson
+        self.unit=unit
+        self.state=str()
+    def get_state(self):
+        return state
