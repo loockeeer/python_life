41e0ae1c8f376ed8cde73240e071baf79fae6a56
loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-02-21 13:16:19 +0100

    Update cake.py and Create recipe.py

--- classe/cake.py
+++ classe/cake.py
@@ -1,8 +1,23 @@
+from time import sleep
 class Cake:
-    def __init__(ingredients=dict(), temps_cuisson=int(), unit=str()):
+    def __init__(name="undefined", ingredients=dict(), temps_cuisson=int(), unit=str()):
         self.ingredients=ingredients
         self.temps_cuisson=temps_cuisson
         self.unit=unit
-        self.state=str()
+        self.state="not cooked"
+        if unit=="s":
+            pass
+        if unit=="m":
+            temps_cuisson=temps_cuisson/60
+        if unit=="h":
+            temps_cuisson=temps_cuisson/60
+            temps_cuisson=temps_cuisson/60
     def get_state(self):
         return state
+    def __str__(self):
+        return self.name
+    def __list__(self):
+        return self.ingredients
+    def cook(self):
+        sleep(self.temps_cuisson)
+        self.state="cooked"
