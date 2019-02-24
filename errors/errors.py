24b17aad4a582992ee7f2e0bfd862a8209da5bc2
Loockeeer <lulu.r1313@gmail.com>
2019-02-24 19:05:33 +0100

    Update errors.py

--- errors/errors.py
+++ errors/errors.py
@@ -3,4 +3,5 @@ class Error(Exception):pass
 class CookTimeError(Error):
     """Raise when cook time is upper than 999999999 seconds"""
 
-
+class NoNameGiven(Error):
+    """Raise when no was given for a person"""
